69f4f917dfff7d2539340c4561cdc8b07097508e
박호동 <sseodjvksgksss@gmail.com>
2022-04-11 17:14:30 +0900

    hodong

---
+++ hohohodong/fe00/ex00/src/App.js
@@ -0,0 +1,33 @@
+import React, { Component } from 'react';
+
+class App extends Component {
+  render() {
+
+    const style = {
+      border : '1px solid black',
+    }
+    return (
+      <div>
+        
+        <section style = {style}>
+        <h1>ToDoList</h1>
+        </section>
+        
+
+        
+        <section>
+          <p>입력할 부분</p>
+        </section>
+       
+
+        <hr></hr>
+        <section>
+          <p>할일 리스트 부분</p>
+        </section>
+        
+      </div>
+    );
+  }
+}
+
+export default App;
